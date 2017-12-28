ffd66be7ff032724a9cf38a66363afac6bbc6585
Roc-J <646481338@qq.com>
2017-12-28 09:45:04 +0800

    find_HSV_value

---
+++ find_HSV_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*- 
+# Author: qjk
+
+import cv2
+import numpy as np
+
+#
+blue = np.uint8([[[255, 0, 0]]])
+hsv_blue = cv2.cvtColor(blue, cv2.COLOR_BGR2HSV)
+print(hsv_blue)
+
+
+green = np.uint8([[[0, 255, 0]]])
+hsv_green = cv2.cvtColor(green, cv2.COLOR_BGR2HSV)
+print(hsv_green)
